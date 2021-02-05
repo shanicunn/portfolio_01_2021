c45d50a6ad9e505dabe8da5b8c957c80e101d116
Shani <36285409+shanicunn@users.noreply.github.com>
2021-02-04 22:46:42 -0500

    Bio blurb height, width, padding, line height etc updated

--- src/components/Edu_Contact_Bio.js
+++ src/components/Edu_Contact_Bio.js
@@ -19,7 +19,8 @@ function Edu_Contact_Bio() {
 
             <section className="contact">
                 <h2 class="section-title" id="main-contact">Contact Me:</h2>
-                <p>SHANI CUNNINGHAM <br />
+                <article>
+                     <p>SHANI CUNNINGHAM <br />
                     <email>shanicunn@gmail.com</email>
                 </p>
                 <h3 id="Logos-contact">
@@ -28,13 +29,19 @@ function Edu_Contact_Bio() {
                 <img src={github} alt="github"/>
                 <img src={instagram} alt="instagram"/>
             </h3>
+                </article>
+               
             </section>
 
             <section className="bio">
                 <h2 class="section-title" id="main-bio">About Me:</h2>
                 <img src={headshot} alt="headshot" id="headshot"/>
                 <aside id="blurb">Welcome to my site! 
-                    My name is Shani (shaw-nee). I am an Atlanta transplant born and raised in Boston, MA with a love for the marrying of technology and creativity.<br/>
+                Originally from Boston, but currently residing just outside of Atlanta, I am a junior software developer with a strong interest in creating innovative programs. <br /> <br />
+
+Out of high school, I went into a Psychology program and graduated Cum Laude with an Associate of Arts. I was introduced to software development while taking Introduction to Programming, a general required course for a baccalaureate program, and was instantly intrigued. My new interest became a hobby as I drowned myself in Linkedin Learning videos/Codecademy videos and then I realized this is something I could proudly do everyday as a professional. <br /> <br />
+
+After two years of software engineering exploration, I was lucky to be accepted into Per Scholas where I was able to specifically sharpen my skills in JavaScript, Java, and CSS. I loved the experience, and I am in search of a role that will enable me to utilize these skills professionally. 
                     
 
 
